04351d621a5d42577b718b52da4c42ed97f1a9b4
Sean <seanms418@gmail.com>
2022-04-18 20:09:45 +1000

    added the header background image and text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import '../styles/Header.css';
 
 export default function Header() {
+    const style = {
+        backgroundImage:`url(${process.env.PUBLIC_URL+ "/header-img.png"})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        color: 'white',
+        fontSize: '30px',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
     return (
-        <div>
+        <div style={style} className="header-container">
             <div>
                 <h1 className="initials">SS</h1>
             </div>
