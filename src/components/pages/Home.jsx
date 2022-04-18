d71198fbfd21df0c3c73bb21e4701fbccde6aa7d
Sean <seanms418@gmail.com>
2022-04-19 01:28:22 +1000

    added the already written header component jsx to the Home component, changed header.css into home.css

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
+import '../../styles/Home.css';
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
-    </div>
-  );
+    const style = {
+        backgroundImage:`url(${process.env.PUBLIC_URL+ "/images/header-img.png"})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        color: 'white',
+        fontSize: '30px',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontWeight: 'bold',
+    }
+    return (
+        <div style={style}>
+            <div>
+                <h1 className="initials">SS</h1>
+            </div>
+            <div className="single-line"></div>
+            <header className="header">
+                <h1>SEAN SCOTT</h1>
+                <p>FULL-STACK WEB DEVELOPER</p>
+            </header>
+        </div>
+    );
 }
