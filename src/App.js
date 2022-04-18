eab6c31abaeacbc1f169d771dd2720e9c6120761
Sean <seanms418@gmail.com>
2022-04-18 18:37:49 +1000

    created the links component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Links from './components/Links';
 
 function App() {
   return (
     <div>
       <Header />
+      <Links />
     </div>
   );
 }
