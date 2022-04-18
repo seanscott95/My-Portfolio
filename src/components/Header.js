67f280cabf777d9bee9a2a59603bc958e819d270
Sean <seanms418@gmail.com>
2022-04-19 01:04:42 +1000

    changed layout design of the portfolio, commented out everything in files not being used instead of deleting to maybe use content later

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import '../styles/Header.css';
+// import React from 'react';
+// import '../styles/Header.css';
 
-export default function Header() {
-    const style = {
-        backgroundImage:`url(${process.env.PUBLIC_URL+ "/images/header-img.png"})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        color: 'white',
-        fontSize: '30px',
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontWeight: 'bold',
-    }
-    return (
-        <div style={style} className="header-container">
-            <div>
-                <h1 className="initials">SS</h1>
-            </div>
-            <div className="single-line"></div>
-            <header className="header">
-                <h1>SEAN SCOTT</h1>
-                <p>FULL-STACK WEB DEVELOPER</p>
-            </header>
-            <div className="single-line"></div>
-        </div>
-    )
-}
+// export default function Header() {
+//     const style = {
+//         backgroundImage:`url(${process.env.PUBLIC_URL+ "/images/header-img.png"})`,
+//         backgroundRepeat: 'no-repeat',
+//         backgroundPosition: 'center',
+//         backgroundSize: 'cover',
+//         color: 'white',
+//         fontSize: '30px',
+//         height: '100vh',
+//         display: 'flex',
+//         flexDirection: 'column',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontWeight: 'bold',
+//     }
+//     return (
+//         <div style={style} className="header-container">
+//             <div>
+//                 <h1 className="initials">SS</h1>
+//             </div>
+//             <div className="single-line"></div>
+//             <header className="header">
+//                 <h1>SEAN SCOTT</h1>
+//                 <p>FULL-STACK WEB DEVELOPER</p>
+//             </header>
+//             <div className="single-line"></div>
+//         </div>
+//     )
+// }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import '../styles/Navbar.css';
+// import React from 'react';
+// import '../styles/Navbar.css';
 
-export default function Links() {
-    return (
-        <div className='navbar'>
-            <a className='navbar-item' href="#projects">PROJECTS</a>
-            <a className='navbar-item' href="#about-me">ABOUT</a>
-            <a className='navbar-item' href="#contact">CONTACT</a>
-            <a className='last-navbar-item' href="#resume">RESUME</a>
-        </div>
-    )
-}
+// export default function Links() {
+//     return (
+//         <div className='navbar'>
+//             <a className='navbar-item' href="#projects">PROJECTS</a>
+//             <a className='navbar-item' href="#about-me">ABOUT</a>
+//             <a className='navbar-item' href="#contact">CONTACT</a>
+//             <a className='last-navbar-item' href="#resume">RESUME</a>
+//         </div>
+//     )
+// }
