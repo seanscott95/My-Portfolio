b25aaca2a166927b83b101efbac709d5d6b3bbe8
Sean <seanms418@gmail.com>
2022-04-18 18:20:05 +1000

    created header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      <p>My Portfolio.</p>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <div>
+            <div>
+                <h1 className="initials">SS</h1>
+            </div>
+            <header className="header">
+                <h1>SEAN SCOTT</h1>
+                <p>FULL-STACK WEB DEVELOPER</p>
+            </header>
+        </div>
+    )
+}
