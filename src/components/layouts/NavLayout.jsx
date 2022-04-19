11ccd1a823503a130fc07261b60d67da74843086
Sean <seanms418@gmail.com>
2022-04-19 14:52:43 +1000

    added some css to the navlayout component

--- src/components/layouts/NavLayout.jsx
+++ src/components/layouts/NavLayout.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../../styles/NavLayout.css'
 
 export default function NavLayout({ currentPage, handlePageChange }) {
 
@@ -25,11 +26,26 @@ export default function NavLayout({ currentPage, handlePageChange }) {
         },
     ];
 
+    const navbarStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        textAlign: 'center',
+        fontSize: '25px',
+        listStyle: 'none',
+        textDecoration: 'none',
+        backgroundImage:`url(${process.env.PUBLIC_URL+ "/images/header-img.png"})`,
+        backgroundRepeat: 'repeat',
+        backgroundPosition: 'center',
+        margin: '0px',
+        padding: '30px',
+
+    }
+
     return (
-        <ul>
+        <ul style={navbarStyle}>
             {navbarPages.map((page) => (
                 <li className={currentPage === page.title ? 'navbarLinkActive' : 'navbarLink'}>
-                    <a href={page.link} onClick={() => handlePageChange(page.title)}>
+                    <a className="anchor" href={page.link} onClick={() => handlePageChange(page.title)}>
                         {page.title}
                     </a>
                 </li>
