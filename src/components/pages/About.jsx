b81c2a71a37e3581ed1199b1db2086767e871a57
Sean <seanms418@gmail.com>
2022-04-20 18:44:10 +1000

    added the css styling for the about page, have a colored in div representing the avatar to be displayed

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../../styles/About.css';
 // import avatar from '../../assets/images/avatar.png';
 
 export default function About() {
@@ -8,25 +9,26 @@ export default function About() {
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
         backgroundSize: 'cover',
-        height: '100vh',
+        height: '77.6vh',
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'center',
         alignItems: 'center',
-        fontWeight: 'bold',
     };
 
     return (
         <div style={style}>
             <h1 className='about-title'>About</h1>
             {/* <image className="avatar" src={avatar} alt="My Avatar"></image> */}
-            <p>
-                I am an aspiring Full Stack Web Developer. My background in Web Development is in the infant
-                stages but I have learnt many relatable and transferable skills through various fields of my
-                career. I learnt Full Stack Web Development through a Monash University Bootcamp and am slowly
-                but surely making my way through this new and crazy world. I am looking to make this my career
-                and am very excited and intrigued to see what comes next.
-            </p>
+            <div className='containerInfo'>
+                <div className='fakeImage'></div>
+                <p className='aboutInfo'>
+                    I am an aspiring Full Stack Web Developer. My background in Web Development is in the infant
+                    stages but I have learnt many relatable and transferable skills through various fields of my
+                    career. I learnt Full Stack Web Development through a Monash University Bootcamp and am slowly
+                    but surely making my way through this new and crazy world. I am looking to make this my career
+                    and am very excited and intrigued to see what comes next.
+                </p>
+            </div>
         </div>
     );
 }
