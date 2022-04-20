5aa5e10c84f846d4351c72ac0d0249b5294b8663
Sean <seanms418@gmail.com>
2022-04-20 21:37:36 +1000

    added styling to the resume component

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import '../../styles/Resume.css';
+import '../../styles/Resume.css';
 
 export default function Resume() {
 
@@ -10,13 +10,33 @@ export default function Resume() {
         backgroundSize: 'cover',
         height: '77.6vh',
         display: 'flex',
-        flexDirection: 'column',
+        flexDirection: 'row',
         alignItems: 'center',
+        justifyContent: 'center',
+        
     };
 
     return (
         <div style={style}>
-            <p>Yo</p>
+            <div className='resumeLink'>
+                <p className='bold'>To save you some time here is a list of my proficiencies:</p>
+                <span>Click here for my resume: </span><button className='resumeBtn'>Resume</button>
+            </div>
+            <div className='resumeInfo'>
+                <p className='resumeLanguages'>
+                    <span className='bold'>Languages: </span> 
+                    JavaScript (AJAX, JSON), HTML, CSS, SQL.
+                </p>
+                <p className='resumeLanguages'>
+                    <span className='bold'>Tools: </span>
+                    VSCode, Git, GitHub, Command Line, Heroku, MySQL, MongoDB, Jest.
+                </p>
+                <p className='resumeLanguages'>
+                    <span className='bold'>Frameworks and Libraries: </span>
+                    React (Hooks), jQuery, Bootstrap, Node.js (Express.js, 
+                    Inquirer.js, File System, Sequelize), Handlebars.js, GraphQL/Apollo.
+                </p>
+            </div>
         </div>
     );
 }
