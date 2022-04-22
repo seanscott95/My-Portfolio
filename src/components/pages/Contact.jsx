25a3b201f2173f2306b73e315825c80e11ac9b45
Sean <seanms418@gmail.com>
2022-04-22 18:40:10 +1000

    added the form inputs values to on change change their state variable using the state variables function

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 import '../../styles/Contact.css';
 
 export default function Contact() {
@@ -15,30 +16,77 @@ export default function Contact() {
     alignItems: 'center',
     fontWeight: 'bold',
     color: '#1e2327',
-    
+
   }
 
-  
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [emailErrorMessage, setEmailErrorMessage] = useState('');
+  const [mouseOutErrorMessage, setMouseOutErrorMessage] = useState('');
 
- 
+  const handleMouseOut = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (!inputValue) {
+      setMouseOutErrorMessage('This field is required.');
+      return;
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(email)) {
+      setEmailErrorMessage('Email is invalid.');
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+    }
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setName('');
+    setEmail('');
+    setMessage('');
+    setEmailErrorMessage('');
+    setMouseOutErrorMessage('');
+  };
 
   return (
     <div style={style}>
       <div className='contactContainer'>
-        <h1 className="title" >Contact Page</h1>
+        <h1 className="white" >Contact Page</h1>
 
         <form className='contactForm'>
           <label for='name'>Name</label>
-          <input type='text' id='name' name='name' placeholder='Your name...' />
-          
+          <input type='text' id='name' name='name' placeholder='Your name...' value={name}
+            onMouseOut={handleMouseOut} onChange={(e) => setName(e.target.value)}/>
+
           <label for='email'>Email</label>
-          <input type='text' id='email' name='email' placeholder='Your email...' />
+          <input type='email' id='email' name='email' placeholder='Your email...'
+            value={email} onMouseOut={handleMouseOut} onChange={(e) => setEmail(e.target.value)}/>
 
           <label for='message'>Message</label>
-          <textarea type='text' id='message' name='message' rows='5' placeholder='Your message...'></textarea>
+          <textarea type='text' id='message' name='message' rows='5'
+            placeholder='Your message...' value={message} onMouseOut={handleMouseOut}
+            onChange={(e) => setMessage(e.target.value)} ></textarea>
 
-          <input type="submit" value="Send" className="sendButton" />
+          <input type="submit" value="Send" className="sendButton" onClick={handleFormSubmit} />
         </form>
+        {setEmailErrorMessage && (
+          <div>
+            <p className="white">{emailErrorMessage}</p>
+          </div>
+        )}
+        {setMouseOutErrorMessage && (
+          <div>
+            <p className="white">{mouseOutErrorMessage}</p>
+          </div>
+        )}
       </div>
     </div>
   );
