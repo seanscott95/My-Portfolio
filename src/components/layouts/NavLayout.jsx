2b20120c6a6d0cdd9dd0641776e8cd65f602c462
Sean <seanms418@gmail.com>
2022-04-22 20:24:28 +1000

    tried adding effect to render aboutpage on first sign up, commented out as it is not working

--- src/components/layouts/NavLayout.jsx
+++ src/components/layouts/NavLayout.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+// import React, { useEffect } from 'react';
+import React from 'react'
 import '../../styles/NavLayout.css';
 
 export default function NavLayout({ currentPage, handlePageChange }) {
+
+    // useEffect(() => {
+    //     // checking if localStorage has a "hasVisited" key
+    //     if (localStorage.getItem("hasVisited")){
+    //         // setting the state of welcomeMessage to "Welcome back!" if it does
+    //         return handlePageChange('#home');
+    //     } else {
+    //         // creating the "hasVisited" key value pair in localStorage if it doesnt exist
+    //         localStorage.setItem("hasVisited", "true");
+    //         return handlePageChange('#about');
+    //     }
+    // },[]);
 
     const navbarPages = [
         {
