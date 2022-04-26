1804573f34b4ebee3b51aca3c16c513b75ac63f5
Sean <seanms418@gmail.com>
2022-04-26 17:46:15 +1000

    removed commented out function

--- src/components/layouts/NavLayout.jsx
+++ src/components/layouts/NavLayout.jsx
@@ -4,18 +4,6 @@ import '../../styles/NavLayout.css';
 
 export default function NavLayout({ currentPage, handlePageChange }) {
 
-    // useEffect(() => {
-    //     // checking if localStorage has a "hasVisited" key
-    //     if (localStorage.getItem("hasVisited")){
-    //         // setting the state of welcomeMessage to "Welcome back!" if it does
-    //         return handlePageChange('#home');
-    //     } else {
-    //         // creating the "hasVisited" key value pair in localStorage if it doesnt exist
-    //         localStorage.setItem("hasVisited", "true");
-    //         return handlePageChange('#about');
-    //     }
-    // },[]);
-
     const navbarPages = [
         {
             title: 'Home',
