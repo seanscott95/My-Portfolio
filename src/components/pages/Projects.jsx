1f45e30d91c938893b3e7284d497e89981696865
Sean <seanms418@gmail.com>
2022-06-23 18:21:00 +1000

    added technologies for projects and edited project descriptions

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -19,11 +19,12 @@ export default function Projects() {
 
   const projects = [
     {
-      title: 'Pet Animal Welfare System (PAWS)',
+      title: 'Pet Advocate Welfare System (PAWS)',
       collaborators: 'Sean Scott, Ayako Woollan and Alan Cherian.',
-      description: 'Pet Booking system for a pet daycare centre. This booking system would be used by the staff of the pet daycare centre.',
-      githubLink: 'https://github.com/Black-Mandarin/Pet-Advocate-Welfare-System-PAWS-',
+      description: 'PAWS provides not only a pet booking system that displays booking information but when a staff members is allocated a booking they can see who they are caring for on their own profile.',
+      githubLink: 'https://github.com/seanscott95/Pet-Advocate-Welfare-System-PAWS-',
       deployedLink: 'https://pet-advocate-welfare-system.herokuapp.com/',
+      technologies: 'HTML, JS, Node, Express, MySQL, Sequelise, Bootstap, Handlebars.',
       image: paws,
       hosted: true,
     },
@@ -33,6 +34,7 @@ export default function Projects() {
       description: 'Login in to view or sign-up to post and comment on a tech blog site where the user can publish useful tech facts or their thoughts and opinions.',
       githubLink: 'https://github.com/seanscott95/Tech-Blog',
       deployedLink: 'https://pacific-ravine-00893.herokuapp.com/',
+      technologies: 'HTML, JS, Node, Express, MySQL, Sequelise, Bootstap, Handlebars.',
       image: techBlog,
       hosted: true,
     },
@@ -42,15 +44,17 @@ export default function Projects() {
       description: 'Enjoy the convenience of having a place to take notes on and offline. This application has the ability to be downloaded onto your computer.',
       githubLink: 'https://github.com/seanscott95/Text-Editor',
       deployedLink: 'https://boiling-wave-43662.herokuapp.com/',
+      technologies: 'HTML, CSS, JS, Node, Express, MySQL, Sequelise.',
       image: jate,
       hosted: true,
     },
     {
       title: 'Social Network API (NoSQL)',
       collaborators: 'Sean Scott.',
-      description: ' A social network API where users can share their thoughts, react to friends’ thoughts, and create a friend list.',
+      description: 'A social network API where users can share their thoughts, react to friends’ thoughts, and create a friend list.',
       githubLink: 'https://github.com/seanscott95/NoSQL-Social-Network-API',
       deployedLink: 'https://drive.google.com/file/d/1tPBQuYIUq_NJp3XgXrDzNpEoy45Q-6la/view',
+      technologies: 'HTML, JS, Node, Express, MongoDB, Mongoose.',
       image: socialNetworkApi,
       hosted: false,
     },
@@ -60,6 +64,7 @@ export default function Projects() {
       description: 'Enter information about employees on a software engineering to generate an HTML webpage which displays summaries for each person.',
       githubLink: 'https://github.com/seanscott95/Team-Profile-Generator',
       deployedLink: 'https://drive.google.com/file/d/1yNBxCo7Cfa1ZD1T4x9U5-uVDR7NTmkKL/view',
+      technologies: 'HTML, JS, Node, Inquirer, Bootstrap.',
       image: teamProfileGenerator,
       hosted: false,
     },
@@ -69,6 +74,7 @@ export default function Projects() {
       description: 'Search for a city via the form input you will be presented with the current and future temperature, humidity, wind speed, and UV index.',
       githubLink: 'https://github.com/seanscott95/Weather-Dashboard',
       deployedLink: 'https://seanscott95.github.io/Weather-Dashboard/',
+      technologies: 'HTML, CSS, JS',
       image: weatherDashboard,
       hosted: false,
     },
@@ -86,6 +92,7 @@ export default function Projects() {
                   <h2>{projects.title}</h2>
                   <p><span className='titleBold'>Collaborators: </span>{projects.collaborators}</p>
                   <p>{projects.description}</p>
+                  <p>Technologies: {projects.technologies}</p>
                   
                   <p className={projects.hosted ? 'note' : 'noteHide'}>Note: App hosted on a free heroku server. Please allow 15-30 seconds to load the page.</p>
                   <a className="projectLinks" href={projects.githubLink}
