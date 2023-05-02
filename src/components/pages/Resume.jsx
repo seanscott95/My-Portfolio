ca706266c2f697188941e351418d6d98096b8e72
Sean <seanms418@gmail.com>
2023-05-02 21:37:30 +1000

    changedd the background, edidtedd the responsiveness, changed some classnames to read easier

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,44 +1,32 @@
 import React from 'react';
-import resumeBackground from '../../assets/images/hills.png';
 import resume from '../../assets/resume/Resume - Sean Scott.pdf'
 import '../../styles/Resume.css';
 
 export default function Resume() {
-
-    const style = {
-        backgroundImage: `url(${resumeBackground})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 'calc(100vh - 200px)',
-    };
-
     return (
-        <div style={style}>
-                <div className='resumeContainer'>
-                    <p className='resumeInner bold'>To save you some time here is a list of my proficiencies:</p>
-                    
-                    <span className="resumeText">Click here for my resume: </span>
-                    <a className='resume' href={resume} target='_blank' rel='noreferrer'>Resume</a>
-                    
-                    <p className='resumeInner'>
-                        <span className='bold'>Languages: </span>
-                        JavaScript (AJAX, JSON), HTML, CSS, SQL.
-                    </p>
-                    <p className='resumeInner'>
-                        <span className='bold'>Tools: </span>
-                        VSCode, Git, GitHub, Command Line, Heroku, MySQL, MongoDB, Jest, RTK.
-                    </p>
-                    <p className='resumeInner'>
-                        <span className='bold'>Frameworks and Libraries: </span>
-                        React (Hooks), jQuery, Bootstrap, Handlebars.js, GraphQL/Apollo, Node.js (Express,
-                        Inquirer, File System, Sequelize, Mongoose, Dotenv).
-                    </p>
-                </div>
+        <div className='resumeContainer'>
+            <div className='infoContainer'>
+                <p>To save you some time here is a list of my proficiencies:</p>
+
+                <span>Click here for my resume: </span>
+                <button>
+                    <a href={resume} target='_blank' rel='noreferrer'>Resume</a>
+                </button>
+
+                <p>
+                    <span>Languages: </span>
+                    JavaScript (AJAX, JSON), HTML, CSS, SQL.
+                </p>
+                <p>
+                    <span>Tools: </span>
+                    VSCode, Git, GitHub, Command Line, Heroku, MySQL, MongoDB, Jest, RTK.
+                </p>
+                <p>
+                    <span>Frameworks and Libraries: </span>
+                    React (Hooks), jQuery, Bootstrap, Handlebars.js, GraphQL/Apollo, Node.js (Express,
+                    Inquirer, File System, Sequelize, Mongoose, Dotenv).
+                </p>
+            </div>
         </div>
     );
 }
