cb15ceceddd52bfadf5fcdd8ac91f231fc170a87
Sean <seanms418@gmail.com>
2023-05-02 17:56:57 +1000

    edited the classnames, deleted some that weren't needed, edited the responsiveness of the component

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,37 +1,21 @@
 import React from 'react';
-import rockBackground from '../../assets/images/rock.png';
 import portrait from '../../assets/images/portrait.png';
 import '../../styles/About.css';
 
 export default function About() {
-
-    const style = {
-        backgroundImage: `url(${rockBackground})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 'calc(100vh - 200px)',
-    };
-
     return (
-        <div style={style}>
-            <div className='containerInfo'>
-                <img src={portrait} alt='avatar' className="avatar" />
-                <div className='containerBackground'>
-                    <h1 className='about-title'>About</h1>
-                    <p className='aboutInfo'>
-                    Full Stack Web Developer leveraging background in Military and Nursing. Effective at 
-                    collaboration and communication within many environments. Completed a certificate in 
-                    Full Stack Web Development from Monash University. Passionate about continuous 
-                    development and improving skills to further advance projects. Looking forward to and are 
+        <div className='aboutContainer'>
+            <img src={portrait} alt='portrait' />
+            <div className='infoContainer'>
+                <h1>About</h1>
+                <p>
+                    Full Stack Web Developer leveraging background in Military and Nursing. Effective at
+                    collaboration and communication within many environments. Completed a certificate in
+                    Full Stack Web Development from Monash University. Passionate about continuous
+                    development and improving skills to further advance projects. Looking forward to and are
                     very excited and intrigued to see what comes next in this line of work.
-                    </p>
-                </div>
+                </p>
             </div>
         </div>
     );
-}
+};
