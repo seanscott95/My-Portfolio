a3b32cbae7a304e205cb6f453c8362359521e6d6
Sean <seanms418@gmail.com>
2023-05-03 15:31:46 +1000

    edited what the about me paragraph says

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -9,11 +9,11 @@ export default function About() {
             <div className='infoContainer'>
                 <h1>About</h1>
                 <p>
-                    Full Stack Web Developer leveraging background in Military and Nursing. Effective at
-                    collaboration and communication within many environments. Completed a certificate in
-                    Full Stack Web Development from Monash University. Passionate about continuous
-                    development and improving skills to further advance projects. Looking forward to and are
-                    very excited and intrigued to see what comes next in this line of work.
+                    I am a Full Stack Web Developer with varied experience in web development projects, healthcare,
+                    customer service and the Australian Defence Force. I hold a Coding Boot Camp Certificate from
+                    Monash University and I am proficient in the use of MERN stack, GraphQL, MySQL,Bootstrap, JQuery
+                    and Redux.  I am passionate about continuous professional development and I am open to varied
+                    opportunities within web development to assist in advancing further projects.
                 </p>
             </div>
         </div>
